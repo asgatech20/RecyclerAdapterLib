7ef7b2c2a4a90059be2663b701b591e89b083fe2
Muhammad Noamany <muhammadnoamany@gmail.com>
2021-10-19 16:12:25 +0200

    ## cresting sample

--- app/src/main/java/com/asgatech/recycleradapterlib/MainActivity.kt
+++ app/src/main/java/com/asgatech/recycleradapterlib/MainActivity.kt
@@ -1,11 +1,47 @@
 package com.asgatech.recycleradapterlib
 
-import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import com.asga.recycler_adapter.BaseAdapter
+import com.asgatech.recycleradapterlib.data.ItemModel
+import com.asgatech.recycleradapterlib.databinding.ActivityMainBinding
+import com.asgatech.recycleradapterlib.databinding.LayoutItemRowBinding
 
 class MainActivity : AppCompatActivity() {
+    private var adapter: BaseAdapter<LayoutItemRowBinding, ItemModel>? = null
+    private var binding: ActivityMainBinding? = null
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+        setAdapter()
+        initMockData()
     }
+
+    private fun setAdapter() {
+        // init the adapter and set assign it as the recycler adapter
+        adapter = BaseAdapter(R.layout.layout_item_row)
+        binding!!.recycler.adapter = adapter
+    }
+
+
+    private fun initMockData() {
+        // set some fake data to be used to test the adapter sample
+        var itemsList = mutableListOf<ItemModel>()
+        itemsList.add(ItemModel("John Doe", "johndoe@gmail.com"))
+        itemsList.add(ItemModel("John Star", "johnstar@gmail.com"))
+        itemsList.add(ItemModel("John Doe", "johndoe@gmail.com"))
+        itemsList.add(ItemModel("John Star", "johnstar@gmail.com"))
+        itemsList.add(ItemModel("John Doe", "johndoe@gmail.com"))
+        itemsList.add(ItemModel("John Star", "johnstar@gmail.com"))
+        itemsList.add(ItemModel("John Doe", "johndoe@gmail.com"))
+        itemsList.add(ItemModel("John Star", "johnstar@gmail.com"))
+        itemsList.add(ItemModel("John Doe", "johndoe@gmail.com"))
+        itemsList.add(ItemModel("John Star", "johnstar@gmail.com"))
+        itemsList.add(ItemModel("John Doe", "johndoe@gmail.com"))
+
+        // pass the data to the adapter
+        adapter!!.setDataList(itemsList)
+    }
+
 }
---
+++ app/src/main/java/com/asgatech/recycleradapterlib/data/ItemModel.kt
@@ -0,0 +1,11 @@
+package com.asgatech.recycleradapterlib.data
+
+/**
+ * @Author: Muhammad Noamany
+ * @Date: 10/19/2021
+ * @Date: muhammadnoamany@gmail.com
+ */
+data class ItemModel(
+    val username: String? = "",
+    val userEmail: String? = "",
+)
